a18ecc4f0225303cadd17a9e051d171b0676aed6
Andrew Bissell <abissell@gmail.com>
2020-05-20 07:01:28 +0000

    ch 10 lifetimes examples

---
+++ ch-10/lifetimes/src/main.rs
@@ -0,0 +1,67 @@
+#![allow(dead_code)]
+#![allow(dead_code)]
+
+fn main() {
+    let string1 = String::from("abcd");
+
+    let result;
+    {
+        let string2 = "xyzabc";
+        result = longest(string1.as_str(), string2);
+    }
+    println!("The longest string is {}", result);
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.')
+        .next()
+        .expect("Could not find a '.'");
+    let _i = ImportantExcerpt { part: first_sentence };
+}
+
+fn longest<'a>(string1: &'a str, string2: &'a str) -> &'a str {
+    if string1.len() > string2.len() {
+        string1
+    } else {
+        string2
+    }
+}
+
+struct ImportantExcerpt<'a> {
+    part: &'a str,
+}
+
+impl<'a> ImportantExcerpt<'a> {
+    fn level(&self) -> i32 {
+        3
+    }
+
+    fn announce_and_return_part(&self, announcement: &str) -> &str {
+        println!("Attention please: {}", announcement);
+        self.part
+    }
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+    where T: Display
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
