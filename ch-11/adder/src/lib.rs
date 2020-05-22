53786dac2df7dbb4cd60d20767b7a51ccae048c0
Andrew Bissell <abissell@gmail.com>
2020-05-22 07:20:01 +0000

    ch 11 finish automated tests

--- ch-11/adder/src/lib.rs
+++ ch-11/adder/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 #[cfg(test)]
 mod tests {
     #[test]
@@ -28,6 +29,34 @@ mod tests {
 
         assert!(!smaller.can_hold(&larger));
     }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{}`", result
+        );
+    }
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+}
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}!", name)
+}
+
+pub fn add_two(a: i32) -> i32 {
+    a + 2
 }
 
 #[derive(Debug)]
@@ -39,5 +68,23 @@ pub struct Rectangle {
 impl Rectangle {
     pub fn can_hold(&self, other: &Rectangle) -> bool {
         self.length > other.length && self.width > other.width
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!("Guess value must be greater than or equal to 1, got {}", value);
+        } else if value > 100 {
+            panic!("Guess value must be less than or equal to 100, got {}", value);
+        }
+
+        Guess {
+            value
+        }
     }
 }
