5431dc41bb652f361addc0e15eab127269d9bd71
Andrew Bissell <abissell@gmail.com>
2020-06-30 07:01:52 +0000

    ch 13 begin closures work

---
+++ ch-13/closures/src/main.rs
@@ -0,0 +1,83 @@
+#![allow(dead_code)]
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+
+    generate_workout(
+        simulated_user_specified_value,
+        simulated_random_number
+    );
+}
+
+use std::thread;
+use std::time::Duration;
+
+fn simulated_expensive_calculation(intensity: u32) -> u32 {
+    println!("calculating slowly ...");
+    thread::sleep(Duration::from_secs(2));
+    intensity
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!(
+            "Today, do {} pushups!",
+            expensive_result.value(&intensity)
+        );
+        println!(
+            "Next, do {} situps!",
+            expensive_result.value(&intensity)
+        );
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(&intensity)
+             );
+        }
+    }
+}
+
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::cmp::Eq;
+struct Cacher<F, I, O>
+    where F: Fn(&I) -> O, I: Hash + Eq
+{
+    calculation: F,
+    values: HashMap<I, O>,
+}
+
+impl<F, I, O> Cacher<F, I, O>
+    where F: Fn(&I) -> O, I: Hash + Eq
+{
+    fn new(calculation: F) -> Cacher<F, I, O> {
+        Cacher {
+            calculation,
+            values: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, arg: &I) -> O {
+        *self.values.entry(*arg).or_insert((self.calculation)(arg))
+    }
+}
+
+#[test]
+fn call_with_different_values() {
+    let mut c = Cacher::new(|a| a);
+
+    let _v1 = c.value(&1);
+    let v2 = c.value(&2);
+
+    assert_eq!(*v2, 2);
+}
