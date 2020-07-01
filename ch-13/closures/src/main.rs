bc8195683ed831c498a137326d63954c34beea78
Andrew Bissell <abissell@gmail.com>
2020-07-01 07:23:24 +0000

    ch 13 finish generifying closure Fn

--- ch-13/closures/src/main.rs
+++ ch-13/closures/src/main.rs
@@ -20,7 +20,7 @@ fn simulated_expensive_calculation(intensity: u32) -> u32 {
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let mut expensive_result = Cacher::new(|num| {
+    let mut expensive_result = Cacher::new(|&num| {
         println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
@@ -29,11 +29,11 @@ fn generate_workout(intensity: u32, random_number: u32) {
     if intensity < 25 {
         println!(
             "Today, do {} pushups!",
-            expensive_result.value(&intensity)
+            expensive_result.value(intensity)
         );
         println!(
             "Next, do {} situps!",
-            expensive_result.value(&intensity)
+            expensive_result.value(intensity)
         );
     } else {
         if random_number == 3 {
@@ -41,7 +41,7 @@ fn generate_workout(intensity: u32, random_number: u32) {
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_result.value(&intensity)
+                expensive_result.value(intensity)
              );
         }
     }
@@ -51,14 +51,14 @@ use std::collections::HashMap;
 use std::hash::Hash;
 use std::cmp::Eq;
 struct Cacher<F, I, O>
-    where F: Fn(&I) -> O, I: Hash + Eq
+    where F: Fn(&I) -> O, I: Hash + Eq, O: Copy
 {
     calculation: F,
     values: HashMap<I, O>,
 }
 
 impl<F, I, O> Cacher<F, I, O>
-    where F: Fn(&I) -> O, I: Hash + Eq
+    where F: Fn(&I) -> O, I: Hash + Eq, O: Copy
 {
     fn new(calculation: F) -> Cacher<F, I, O> {
         Cacher {
@@ -67,17 +67,24 @@ impl<F, I, O> Cacher<F, I, O>
         }
     }
 
-    fn value(&mut self, arg: &I) -> O {
-        *self.values.entry(*arg).or_insert((self.calculation)(arg))
+    fn value(&mut self, arg: I) -> O {
+        match self.values.get(&arg) {
+            Some(v) => *v,
+            None => {
+                let v = (self.calculation)(&arg);
+                self.values.insert(arg, v);
+                v
+            }
+        }
     }
 }
 
 #[test]
 fn call_with_different_values() {
-    let mut c = Cacher::new(|a| a);
+    let mut c = Cacher::new(|&a| a);
 
-    let _v1 = c.value(&1);
-    let v2 = c.value(&2);
+    let _v1 = c.value(1);
+    let v2 = c.value(2);
 
-    assert_eq!(*v2, 2);
+    assert_eq!(v2, 2);
 }
